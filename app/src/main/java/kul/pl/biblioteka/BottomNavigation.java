2e61017c5ebe64fadc1e9d7ec614645c7abeed88
mrozowski <mrozowskion@gmail.com>
2021-03-12 18:01:46 +0100

    Add navigation bar & profile

---
+++ app/src/main/java/kul/pl/biblioteka/BottomNavigation.java
@@ -0,0 +1,14 @@
+package kul.pl.biblioteka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BottomNavigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+    }
+}
