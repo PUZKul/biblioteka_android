5ec74aeb734548e60f156c4ccffb1aa32c60129f
mrozowski <mrozowskion@gmail.com>
2021-03-12 18:12:33 +0100

    Connect profile to navigation bar

--- app/src/main/java/kul/pl/biblioteka/BottomNavigation.java
+++ app/src/main/java/kul/pl/biblioteka/BottomNavigation.java
@@ -1,14 +1,46 @@
 package kul.pl.biblioteka;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import kul.pl.biblioteka.profile.Profile;
 
 public class BottomNavigation extends AppCompatActivity {
 
+    private BottomNavigationView bottom_nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
+        bottom_nav = findViewById(R.id.bottom_nav);
+        bottom_nav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new Profile()).addToBackStack(null).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()){
+                        case R.id.bottom_nav_profile:
+                            selectedFragment = new Profile();
+                            break;
+                        case R.id.bottom_nav_book:
+                            //selectedFragment = new NewPostSection();
+                            break;
+                        case R.id.bottom_nav_home:
+                            //selectedFragment = new HomeView();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, selectedFragment).addToBackStack(null).commit();
+                    return true;
+                }
+            };
 }
