e689d5dc2fa3824267a1345a86d3345a01cae88c
Szfedziaczek <krystainszwedo@wp.pl>
2021-03-23 19:55:24 +0100

    added HomeFragment.java

--- app/src/main/java/kul/pl/biblioteka/ui/fragments/home/HomeFragment.java
+++ app/src/main/java/kul/pl/biblioteka/ui/fragments/home/HomeFragment.java
@@ -5,17 +5,28 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 import kul.pl.biblioteka.R;
+import kul.pl.biblioteka.adapter.OnItemClickListener;
+import kul.pl.biblioteka.adapter.homeList.HomeListRecycleViewAdapter;
+import kul.pl.biblioteka.models.BookModel;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements HomeFragmentContract.View {
 
     private ImageButton sortBtn;
     private PopupMenu menu;
+    private RecyclerView recyclerView;
+    private EditText searchExitText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -50,10 +61,36 @@ public class HomeFragment extends Fragment {
         sortBtn = view.findViewById(R.id.home_btn_sort);
         menu = new PopupMenu(view.getContext(), sortBtn);
         menu.getMenuInflater().inflate(R.menu.sort_list_menu, menu.getMenu());
+        recyclerView=view.findViewById(R.id.home_resycleView);
+        searchExitText=view.findViewById(R.id.home_searchView_search);
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void setList(List<BookModel> booksList) {
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1, GridLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(new HomeListRecycleViewAdapter(getContext(), booksList, onItemClickListener));
+    }
+
+    private OnItemClickListener onItemClickListener=new OnItemClickListener() {
+        @Override
+        public void onClick() {
+            //toDo go to the bookView add idBook in parameters
+        }
+    };
+
+    @Override
+    public void showToast(String text) {
+        Toast.makeText(getContext(),text,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public String getSearchText() {
+        return searchExitText.getText().toString();
     }
 }
