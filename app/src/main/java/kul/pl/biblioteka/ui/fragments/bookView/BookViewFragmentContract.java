533085d6784de43eb83f31398f11040c8c0086d3
Szfedziaczek <krystainszwedo@wp.pl>
2021-03-23 19:57:25 +0100

    added BookViewFragmentContract.java

---
+++ app/src/main/java/kul/pl/biblioteka/ui/fragments/bookView/BookViewFragmentContract.java
@@ -0,0 +1,29 @@
+package kul.pl.biblioteka.ui.fragments.bookView;
+
+import android.net.Uri;
+
+public interface BookViewFragmentContract {
+    interface View {
+        void setAuthor(String author);
+
+        void setPublisher(String publisher);
+
+        void setPages(String pages);
+
+        void setImage(Uri uriImage);
+
+        void setDate(String date);
+
+        void setAvailable(String available);
+
+        void setStars(double number);
+
+        void startProgressBar();
+
+        void endProgressBar();
+    }
+
+    interface Presenter {
+        void setBookDetails();
+    }
+}
