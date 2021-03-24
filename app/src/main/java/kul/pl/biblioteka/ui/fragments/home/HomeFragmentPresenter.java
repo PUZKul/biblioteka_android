06b6c5f1411e498d8e18d5d3c073b18eef6e1634
Szfedziaczek <krystainszwedo@wp.pl>
2021-03-24 19:06:55 +0100

    implemented interface HomeFragmentContract.Presenter in HomeFragmentPreseter

--- app/src/main/java/kul/pl/biblioteka/ui/fragments/home/HomeFragmentPresenter.java
+++ app/src/main/java/kul/pl/biblioteka/ui/fragments/home/HomeFragmentPresenter.java
@@ -1,4 +1,44 @@
 package kul.pl.biblioteka.ui.fragments.home;
 
-public class HomeFragmentPresenter {
+import kul.pl.biblioteka.api.API;
+
+public class HomeFragmentPresenter implements HomeFragmentContract.Presenter {
+
+    private HomeFragmentContract.View view;
+    private API api;
+
+    public HomeFragmentPresenter(HomeFragmentContract.View view) {
+        this.view = view;
+        this.api = new API();
+    }
+
+    @Override
+    public void setListSortByTitle() {
+        // view.setList();
+    }
+
+    @Override
+    public void setListSortByRating() {
+        //view.setList();
+    }
+
+    @Override
+    public void setListSortByDate() {
+        //view.setList();
+    }
+
+    @Override
+    public void setListTopBooks() {
+        //view.setList();
+    }
+
+    @Override
+    public void setListSortByDiscover() {
+        //view.setList();
+    }
+
+    @Override
+    public void setListByName(String bookName) {
+        //view.setList();
+    }
 }
