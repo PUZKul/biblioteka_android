44129f15505620a3e9a4a3ac9758aac6a25960bd
Szfedziaczek <krystainszwedo@wp.pl>
2021-03-24 18:45:44 +0100

    implement interface HomeFragmentContract.View in HomeFragment

--- app/src/main/java/kul/pl/biblioteka/ui/fragments/home/HomeFragment.java
+++ app/src/main/java/kul/pl/biblioteka/ui/fragments/home/HomeFragment.java
@@ -5,9 +5,9 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -20,19 +20,22 @@ import kul.pl.biblioteka.R;
 import kul.pl.biblioteka.adapter.OnItemClickListener;
 import kul.pl.biblioteka.adapter.homeList.HomeListRecycleViewAdapter;
 import kul.pl.biblioteka.models.BookModel;
+import kul.pl.biblioteka.ui.fragments.bookView.BookViewFragment;
 
 public class HomeFragment extends Fragment implements HomeFragmentContract.View {
 
     private ImageButton sortBtn;
     private PopupMenu menu;
     private RecyclerView recyclerView;
-    private EditText searchExitText;
+    private SearchView searchExitText;
+    private HomeFragmentPresenter presenter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         initComponents(view);
         setOnClickListener();
+        presenter = new HomeFragmentPresenter(this);
         return view;
     }
 
@@ -52,7 +55,17 @@ public class HomeFragment extends Fragment implements HomeFragmentContract.View 
             new PopupMenu.OnMenuItemClickListener() {
                 @Override
                 public boolean onMenuItemClick(MenuItem item) {
-                    //To Do set lists
+                    if (item.getGroupId() == R.id.bottom_sort_title) {
+                        presenter.setListSortByTitle();
+                    } else if (item.getGroupId() == R.id.bottom_sort_date) {
+                        presenter.setListSortByDate();
+                    } else if (item.getGroupId() == R.id.bottom_sort_discover) {
+                        presenter.setListSortByDiscover();
+                    } else if (item.getGroupId() == R.id.bottom_sort_ranting) {
+                        presenter.setListSortByRating();
+                    } else {
+                        presenter.setListTopBooks();
+                    }
                     return true;
                 }
             };
@@ -61,10 +74,9 @@ public class HomeFragment extends Fragment implements HomeFragmentContract.View 
         sortBtn = view.findViewById(R.id.home_btn_sort);
         menu = new PopupMenu(view.getContext(), sortBtn);
         menu.getMenuInflater().inflate(R.menu.sort_list_menu, menu.getMenu());
-        recyclerView=view.findViewById(R.id.home_resycleView);
-        searchExitText=view.findViewById(R.id.home_searchView_search);
+        recyclerView = view.findViewById(R.id.home_resycleView);
+        searchExitText = view.findViewById(R.id.home_searchView_search);
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -77,20 +89,33 @@ public class HomeFragment extends Fragment implements HomeFragmentContract.View 
         recyclerView.setAdapter(new HomeListRecycleViewAdapter(getContext(), booksList, onItemClickListener));
     }
 
-    private OnItemClickListener onItemClickListener=new OnItemClickListener() {
+    private OnItemClickListener onItemClickListener = new OnItemClickListener() {
         @Override
-        public void onClick() {
-            //toDo go to the bookView add idBook in parameters
+        public void onClick(String idBook) {
+            openBookViewFragment(idBook);
         }
     };
 
+    private void openBookViewFragment(String idBook) {
+        Bundle bundle = new Bundle();
+        bundle.putString("idBook", idBook);
+//TODO change code and add method
+        BookViewFragment bookViewFragment = new BookViewFragment();
+        bookViewFragment.setArguments(bundle);
+
+        getActivity().getSupportFragmentManager().beginTransaction().
+                add(((ViewGroup) getView().getParent()).getId(), new BookViewFragment(), idBook)
+                .addToBackStack(getView().getClass().getName())
+                .commit();
+    }
+
     @Override
     public void showToast(String text) {
-        Toast.makeText(getContext(),text,Toast.LENGTH_LONG).show();
+        Toast.makeText(getContext(), text, Toast.LENGTH_LONG).show();
     }
 
     @Override
     public String getSearchText() {
-        return searchExitText.getText().toString();
+        return searchExitText.toString();
     }
 }
