cf3c5813341ddd49e777e03bec0b415e4fba9ddb
mrozowski <mrozowskion@gmail.com>
2021-03-28 23:11:49 +0200

    Added pagination bar to home presenter

--- app/src/main/java/kul/pl/biblioteka/ui/fragments/home/HomeFragmentPresenter.java
+++ app/src/main/java/kul/pl/biblioteka/ui/fragments/home/HomeFragmentPresenter.java
@@ -1,43 +1,68 @@
 package kul.pl.biblioteka.ui.fragments.home;
 
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import java.util.List;
 
 import kul.pl.biblioteka.dataAccess.APIListener;
 import kul.pl.biblioteka.dataAccess.LibraryAccess;
 import kul.pl.biblioteka.exception.ApiError;
 import kul.pl.biblioteka.models.BookModel;
+import kul.pl.biblioteka.utils.PaginationBar;
 import kul.pl.biblioteka.utils.Sorting;
+
+import static kul.pl.biblioteka.utils.Constants.LIMIT;
 
 public class HomeFragmentPresenter implements HomeFragmentContract.Presenter, APIListener {
 
     private HomeFragmentContract.View view;
     private LibraryAccess api;
-
+    private PaginationBar pageBar;
+    private Sorting currentSorting;
 
     public HomeFragmentPresenter(HomeFragmentContract.View view) {
         this.view = view;
         this.api = LibraryAccess.getInstance();
         api.setListener(this);
+        setPaginationComponent();
+    }
+
+    private void setPaginationComponent() {
+        pageBar = new PaginationBar(((Fragment) view).getActivity());
+        pageBar.setOnNextClickListener(nextClickListener);
+        pageBar.setOnPreviousClickListener(previousClickListener);
+        pageBar.setOnPageClickListener(pageClickListener);
     }
 
     @Override
     public void setListSortByTitle() {
-        api.getBooks(10,1, Sorting.TITLE);
+        currentSorting = Sorting.TITLE;
+        api.getBooks(LIMIT,0, currentSorting);
     }
 
     @Override
     public void setListSortByRating() {
-        api.getBooks(10,1, Sorting.RATING);
+        currentSorting = Sorting.RATING;
+        api.getBooks(LIMIT,0, currentSorting);
     }
 
     @Override
     public void setListSortByDate() {
-        api.getBooks(10,1, Sorting.YEAR);
+        currentSorting = Sorting.YEAR;
+        api.getBooks(LIMIT,0, currentSorting);
     }
 
     @Override
     public void setListTopBooks() {
-        api.getBooks(10,1, Sorting.POPULARITY);
+        currentSorting = Sorting.POPULARITY;
+        api.getBooks(LIMIT,0, currentSorting);
     }
 
     @Override
@@ -66,4 +91,31 @@ public class HomeFragmentPresenter implements HomeFragmentContract.Presenter, AP
     public void onBookReceive(BookModel book) {
 
     }
+
+    // listeners for pagination bar
+    private View.OnClickListener previousClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            api.getBooks(LIMIT, pageBar.previousPage(), currentSorting);
+        }
+    };
+
+    private View.OnClickListener nextClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            api.getBooks(LIMIT, pageBar.nextPage(), currentSorting);
+        }
+    };
+
+
+    private final View.OnClickListener pageClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            TextView text = ((Activity) view).findViewById(v.getId());
+            String value = text.getText().toString();
+            int clickedPage = Integer.parseInt(value) - 1;
+            api.getBooks(LIMIT, clickedPage, currentSorting);
+        }
+    };
+
 }
---
+++ app/src/main/java/kul/pl/biblioteka/utils/Constants.java
@@ -0,0 +1,5 @@
+package kul.pl.biblioteka.utils;
+
+public class Constants {
+    public static final int LIMIT = 10;
+}
