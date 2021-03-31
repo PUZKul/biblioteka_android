2912d76007cc0f29333cf1efa15c5486f9d40f9c
Szfedziaczek <krystainszwedo@wp.pl>
2021-03-31 22:56:06 +0200

    created APIAdapter

---
+++ app/src/main/java/kul/pl/biblioteka/dataAccess/APIAdapter.java
@@ -0,0 +1,23 @@
+package kul.pl.biblioteka.dataAccess;
+
+import kul.pl.biblioteka.exception.ApiError;
+import kul.pl.biblioteka.models.BookModel;
+import kul.pl.biblioteka.utils.PageHolder;
+
+public abstract class APIAdapter implements APIListener {
+
+    @Override
+    public void onBookListReceive(PageHolder<BookModel> page) {
+
+    }
+
+    @Override
+    public void onBookReceive(BookModel book) {
+
+    }
+
+    @Override
+    public void onErrorReceive(ApiError error) {
+
+    }
+}
