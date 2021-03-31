c9a1c9db68fef0e40154eac85f6c18ca4fa10bd9
Szfedziaczek <krystainszwedo@wp.pl>
2021-03-31 17:33:03 +0200

    added methods to interface LoginActivityContract

--- app/src/main/java/kul/pl/biblioteka/ui/activity/login/LoginActivityContract.java
+++ app/src/main/java/kul/pl/biblioteka/ui/activity/login/LoginActivityContract.java
@@ -1,4 +1,17 @@
 package kul.pl.biblioteka.ui.activity.login;
 
+import kul.pl.biblioteka.models.UserModel;
+
 public interface LoginActivityContract {
+    interface View {
+        void showToast(String message);
+
+        void startProgressBar();
+
+        void endProgressBar();
+    }
+
+    interface Presenter {
+        void onLoginClicked(UserModel user);
+    }
 }
