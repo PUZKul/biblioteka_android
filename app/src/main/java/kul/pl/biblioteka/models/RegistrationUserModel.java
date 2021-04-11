494d759b7b86a6ef8d6d603dbb4a11d35f87084a
Szfeduś <krystianszwedo@wp.pl>
2021-04-11 23:22:20 +0200

    added class RegistrationUserModel.java

--- app/src/main/java/kul/pl/biblioteka/models/RegistrationUserModel.java
+++ app/src/main/java/kul/pl/biblioteka/models/RegistrationUserModel.java
@@ -1,4 +1,48 @@
 package kul.pl.biblioteka.models;
 
-public class RegistraionUserModel {
+public class RegistrationUserModel {
+
+    private String nick;
+    private String passwordFirst;
+    private String passwordSecond;
+    private String email;
+
+    public RegistrationUserModel(String nick, String passwordFirst, String passwordSecond, String email) {
+        this.nick = nick;
+        this.passwordFirst = passwordFirst;
+        this.passwordSecond = passwordSecond;
+        this.email = email;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getPasswordFirst() {
+        return passwordFirst;
+    }
+
+    public void setPasswordFirst(String passwordFirst) {
+        this.passwordFirst = passwordFirst;
+    }
+
+    public String getPasswordSecond() {
+        return passwordSecond;
+    }
+
+    public void setPasswordSecond(String passwordSecond) {
+        this.passwordSecond = passwordSecond;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
