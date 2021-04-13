58790cffa3cdc73128fdfc3157c9f17c46447c89
IvannaIlchyna <ilchyna31@gmail.com>
2021-04-13 13:12:15 +0200

    Fragments: History and Reading without login in

---
+++ app/src/main/java/kul/pl/biblioteka/ui/fragments/NoLoginReadingAndHistory/NoLoginHistoryFragment.java
@@ -0,0 +1,24 @@
+package kul.pl.biblioteka.ui.fragments.NoLoginReadingAndHistory;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import kul.pl.biblioteka.R;
+
+
+public class NoLoginHistoryFragment extends Fragment {
+    public NoLoginHistoryFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_no_login_history, container, false);
+    }
+}
---
+++ app/src/main/java/kul/pl/biblioteka/ui/fragments/NoLoginReadingAndHistory/NoLoginReadingAndHistory_Main.java
@@ -0,0 +1,20 @@
+package kul.pl.biblioteka.ui.fragments.NoLoginReadingAndHistory;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import kul.pl.biblioteka.R;
+
+public class NoLoginReadingAndHistory_Main extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_no_log_reading_and_history__main, container, false);
+    }
+}
