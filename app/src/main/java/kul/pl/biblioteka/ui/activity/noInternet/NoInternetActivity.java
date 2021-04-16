68dedab5357c72709c7bf15908e8fcc5f734df6c
Szfeduś <krystianszwedo@wp.pl>
2021-04-16 20:50:36 +0200

    added noInternetActivity

---
+++ app/src/main/java/kul/pl/biblioteka/ui/activity/noInternet/NoInternetActivity.java
@@ -0,0 +1,29 @@
+package kul.pl.biblioteka.ui.activity.noInternet;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import kul.pl.biblioteka.R;
+
+public class NoInternetActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.no_internet);
+        button = findViewById(R.id.no_internet_button_back);
+        button.setOnClickListener(onBackClick);
+    }
+
+    private View.OnClickListener onBackClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
+}
