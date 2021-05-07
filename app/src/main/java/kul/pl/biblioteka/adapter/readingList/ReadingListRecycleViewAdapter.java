13f5926bf1a7a640b81fcdc7d10592b2d69c65b5
Szfeduś <krystianszwedo@wp.pl>
2021-05-07 13:50:34 +0200

    implemented reading  book list

---
+++ app/src/main/java/kul/pl/biblioteka/adapter/readingList/ReadingListRecycleViewAdapter.java
@@ -0,0 +1,52 @@
+package kul.pl.biblioteka.adapter.readingList;
+
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import kul.pl.biblioteka.R;
+import kul.pl.biblioteka.models.HistoryBookModel;
+import kul.pl.biblioteka.ui.activity.MainActivity;
+import kul.pl.biblioteka.utils.Helper;
+
+public class ReadingListRecycleViewAdapter extends RecyclerView.Adapter<ReadingListViewHolder> {
+
+
+    private List<HistoryBookModel> booksList;
+
+    public ReadingListRecycleViewAdapter(List<HistoryBookModel> booksList) {
+        this.booksList = booksList;
+    }
+
+    @NonNull
+    @Override
+    public ReadingListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater mInflater = LayoutInflater.from(MainActivity.getAppContext());
+        View view = mInflater.inflate(R.layout.item_list_reading, parent, false);
+        return new ReadingListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReadingListViewHolder holder, int position) {
+        holder.setBorrowedText(Helper.getShortDate(booksList.get(position).getDateIssued()));
+        holder.setImageBook(Uri.parse(booksList.get(position).getImageUrl()));
+        holder.setTitleText(booksList.get(position).getTitle());
+        //todo add static method(Holder class) to added 30 day
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            String date= LocalDate.parse(Helper.getDefaultDateFormat(booksList.get(position).getDateIssued())).plusDays(30).toString();
+            holder.setTimeAgoText(date);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return booksList.size();
+    }
+}
--- app/src/main/java/kul/pl/biblioteka/adapter/readingList/ReadingListViewHolder.java
+++ app/src/main/java/kul/pl/biblioteka/adapter/readingList/ReadingListViewHolder.java
@@ -2,6 +2,7 @@ package kul.pl.biblioteka.adapter.readingList;
 
 import android.net.Uri;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -9,23 +10,25 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
 import kul.pl.biblioteka.R;
 import kul.pl.biblioteka.adapter.OnItemClickListener;
+import kul.pl.biblioteka.ui.activity.MainActivity;
 
-public class ReadingListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+public class ReadingListViewHolder extends RecyclerView.ViewHolder{
 
     private ImageView imageBook;
     private TextView titleTextView;
     private RatingBar ratingBar;
     private  TextView borrowedTextView;
     private TextView timeAgoTextView;
-    private OnItemClickListener onItemClickListener;
 
-    public ReadingListViewHolder(@NonNull View itemView, OnItemClickListener onItemClickListener) {
+    public ReadingListViewHolder(@NonNull View itemView) {
         super(itemView);
         setComponents(itemView);
-        this.onItemClickListener=onItemClickListener;
     }
+
 
     private void setComponents(View view) {
         imageBook=view.findViewById(R.id.reading_item_list_textView_title);
@@ -34,7 +37,7 @@ public class ReadingListViewHolder extends RecyclerView.ViewHolder implements Vi
         timeAgoTextView=view.findViewById(R.id.reading_item_list_textView_timeAgo);
     }
 
-    private void setTimeAgoText(String timeAgoText){
+    public void setTimeAgoText(String timeAgoText){
         timeAgoTextView.setText(timeAgoText);
     }
 
@@ -51,11 +54,7 @@ public class ReadingListViewHolder extends RecyclerView.ViewHolder implements Vi
     }
 
     public void setImageBook(Uri bookUri){
-        imageBook.setImageURI(bookUri);
+        Picasso.with(MainActivity.getAppContext()).load(bookUri).into(imageBook);;
     }
 
-    @Override
-    public void onClick(View v) {
-        //onItemClickListener.onClick();
-    }
 }
