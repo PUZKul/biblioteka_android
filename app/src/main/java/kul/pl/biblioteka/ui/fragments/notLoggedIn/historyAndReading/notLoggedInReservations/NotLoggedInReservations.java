4871b4d80a639df0eecbd8ee61f6fbbc58b5e7d6
Szfeduś <krystianszwedo@wp.pl>
2021-05-10 16:36:31 +0200

    refactored no internet history/reading/reservation

--- app/src/main/java/kul/pl/biblioteka/ui/fragments/notLoggedIn/historyAndReading/notLoggedInReservations/NotLoggedInReservations.java
+++ app/src/main/java/kul/pl/biblioteka/ui/fragments/notLoggedIn/historyAndReading/notLoggedInReservations/NotLoggedInReservations.java
@@ -1,23 +1,46 @@
 package kul.pl.biblioteka.ui.fragments.notLoggedIn.historyAndReading.notLoggedInReservations;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import kul.pl.biblioteka.R;
+import kul.pl.biblioteka.ui.activity.login.LoginActivity;
 
 public class NotLoggedInReservations extends Fragment {
 
-    public NotLoggedInReservations() {
-    }
+    private ImageView loginImage;
+    private TextView loginText;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_no_login_reservations, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_no_login_reservations, container, false);
+        initComponents(view);
+        setOnClickListener();
+        return view;
     }
+
+    private void initComponents(View view) {
+        loginImage=view.findViewById(R.id.notLoggedInReservations_image_LoginIn);
+        loginText=view.findViewById(R.id.notLoggedInReservations_text_LoginIn);
+    }
+
+    private void setOnClickListener(){
+        loginText.setOnClickListener(onLoginClicked);
+        loginImage.setOnClickListener(onLoginClicked);
+    }
+
+    private View.OnClickListener onLoginClicked =new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+        }
+    };
 }
